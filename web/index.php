dda61f62c7731b7c9451e523f9467d40a16198cb
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-09-04 21:47:18 -0700

    first commit

--- web/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // not actually sure, but it has something to do with the autoload.php line in app.php?
-    $website = require_once __DIR__.'/../app/app.php';
-    // then runs 
-    $website->run();
-?>
